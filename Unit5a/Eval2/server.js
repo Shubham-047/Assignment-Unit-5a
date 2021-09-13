97cea1daba6a45ca3083a1385dcd0b9148500f82
Shubham Kumar <shubham1406kumar@gmail.com>
2021-09-13 14:45:08 +0530

    2nd commit

--- Unit5a/Eval2/server.js
+++ Unit5a/Eval2/server.js
@@ -3,4 +3,14 @@ const express = require("express")
 const mongoose = require("mongoose")
 
 
-const app = require(express);
+const app = express();
+
+
+
+
+
+
+app.listen(3045, function () {
+   // await mongoose.connect();
+    console.log("listening to port 3045")
+})
