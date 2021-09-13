6a3471b7fe2813b75adb65e3fdd2892e670532b2
Shubham Kumar <shubham1406kumar@gmail.com>
2021-09-13 16:48:07 +0530

    8th commit

--- Unit5a/Eval2/server.js
+++ Unit5a/Eval2/server.js
@@ -44,45 +44,58 @@ app.get("/users", async(req, res) => {
     res.send(user)
    
 })
-
+//to add users
 app.post("/users", async (req, res) => {
     const user = await User.create(req.body);
     return res.status(201).json({ user });
 })
 
+
+//to get user greater than 18
 app.get("/users/age" ,async (req, res) => {
     const user = await User.find({ age: { $gt: 18 } }).lean().exec();
     return res.status(200).json({ user });
 })
 
+
+//to get user opted full-stack
 app.get("/users/course", async (req, res) => {
      const user = await User.find({ course: { $eq: "Full-Stack" } }).lean().exec();
      return res.status(200).json({ user });
 })
 
 
+
+
+//to get men users
 app.get("/users/gender/male", async (req, res) => {
      const user = await User.find({ gender: { $eq: "male" } }).count().lean().exec();
      return res.status(200).json({ user });
 })
 
 
+
+// to get female users
 app.get("/users/gender/female", async (req, res) => {
      const user = await User.find({ gender: { $eq: "female" } }).count().lean().exec();
      return res.status(200).json({ user });
 })
 
+
+//to get total number of users
 app.get("/users/count", async (req, res) => {
      const user = await User.find().count().lean().exec();
      return res.status(200).json({ user });
 })
 
+
+//to update the data
 app.patch("/users/:id" ,async (req, res) => {
     const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true});
      return res.status(200).json({ user });
 })
 
-
+//to delete the data
 app.delete("/users/:id" ,async (req, res) => {
     const user = await User.findByIdAndDelete(req.params.id);
      return res.status(200).json({ user });
