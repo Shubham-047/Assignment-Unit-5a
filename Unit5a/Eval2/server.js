fe5d72b75e401b912e175da8b6cfc2b72722b9d7
Shubham Kumar <shubham1406kumar@gmail.com>
2021-09-13 15:06:39 +0530

    3rd commit

--- Unit5a/Eval2/server.js
+++ Unit5a/Eval2/server.js
@@ -2,8 +2,17 @@ const express = require("express")
 
 const mongoose = require("mongoose")
 
-
+const connect = () => {
+    return mongoose.connect(" mongodb://127.0.0.1:27017");
+}
 const app = express();
+
+//for getting all users
+app.get("/", (req, res) => {
+    //console.log(users)
+    const a = "user"
+    res.send(a)
+})
 
 
 
