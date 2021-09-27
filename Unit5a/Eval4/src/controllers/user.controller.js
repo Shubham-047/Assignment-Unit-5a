feb22beb84fe97bf2f4759d036ca1e995b30bb71
Shubham Kumar <shubham1406kumar@gmail.com>
2021-09-27 16:12:12 +0530

    6th commit

--- Unit5a/Eval4/src/controllers/user.controller.js
+++ Unit5a/Eval4/src/controllers/user.controller.js
@@ -1,5 +1,23 @@
 const express = require("express");
 
 const router = express.Router();
+const User = require("../model/user.model");
+const { single } = require("../utils/file_uploads");
+const upload = require("../utils/file_uploads")
 
-router.post("/")
+router.post("/single", upload.single("profile_pic"), async (req, res) => {
+    console.log(req.file);
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        roles: req.body.roles,
+         profile_pic_url: req.body. profile_pic_url
+    })
+    return res.status(201).json({data: user})
+
+})
+
+// router.post("/")
+
+module.exports = router;
--- Unit5a/Eval4/src/server.js
+++ Unit5a/Eval4/src/server.js
@@ -2,7 +2,14 @@ const express = require("express")
 
 const app = express();
 const connect = require("./config/db")
+const userController = require("./controllers/user.controller")
+const studentController = require("./controllers/student.controller")
+const lectureController = require("./controllers/lecture.controller")
+
 app.use(express.json());
+// app.use("/users", userController)
+// app.use("/student", studentController)
+// app.use("/lectures", lectureController)
 
 app.listen(2457, async function (req, res) {
     await connect();
